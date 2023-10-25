a06cfab79ef21b2f9867c24f75868166ebaeb60d
RockeyWereWolf <mi71k9d7p@code.edu.az>
2023-10-25 11:05:42 +0400

    uploaded project

---
+++ WeaponTask/Models/Weapon.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponTask.Models
+{
+    internal class Weapon
+    {
+        bool _isValid = true;
+        public bool IsValid { get => _isValid; 
+            set
+            {
+               _isValid = value;
+            } 
+        }
+        double _clipSize;
+        public double ClipSize { get => _clipSize; 
+            set
+            {
+                if (value < 0)
+                {
+                    _isValid = false;
+                    Console.WriteLine("Enter a valid clip size!");
+                    return;
+                }
+                _clipSize = value;
+            }
+        }
+        double _bulletCount;
+        public double BulletCount { get =>_bulletCount; 
+            set
+            {
+                if (BulletCount < 0 || BulletCount > ClipSize)
+                {
+                    IsValid = false;
+                    Console.WriteLine("Enter a valid bullet count!");
+                    return;
+                }
+                _bulletCount = value;
+            } 
+        }
+        double _dischargeSeconds;
+        public double DischargeSeconds { get=> _dischargeSeconds; 
+            set
+            {
+                if (value < 0)
+                {
+                    IsValid = false;
+                    Console.WriteLine("Enter a valid seconds for discharge!");
+                    return;
+                }
+                _dischargeSeconds = value;
+            } 
+        }
+        string _shootMode;
+        public string ShootMode { get=> _shootMode; 
+            set
+            {
+                if (!(value == "auto" || value == "single"))
+                {
+                    IsValid = false;
+                    Console.WriteLine("Enter a valid bullet count!");
+                    return;
+                }
+                _shootMode = value;
+            }
+        }
+
+        public void Shoot()
+        {
+            Console.WriteLine("Shooted with one bullet");
+            BulletCount--;
+        }
+
+        public void Fire()
+        {
+            if (ShootMode == "auto")
+            {
+                Console.WriteLine($"Clip became empty in {(BulletCount/ClipSize)*DischargeSeconds} seconds");
+                BulletCount = 0;
+                return;
+            }
+            Console.WriteLine("Shooted with one bullet");
+            BulletCount--;
+        }
+
+        public double GetNeededBulletCount ()
+        {
+            return ClipSize - BulletCount;
+        }
+
+        public void Reload()
+        {
+            BulletCount = ClipSize;
+        }
+
+        public void ChangeFireMode()
+        {
+            if ( ShootMode == "auto" )
+            {
+                ShootMode = "single";
+            } else if ( ShootMode == "single")
+            {
+                ShootMode = "auto";
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Clipsize of your weapon:{ClipSize}, Your current bullet count: {BulletCount}, Full clip becomes empty in {DischargeSeconds}, Current shoot mode: {ShootMode}";
+        }
+
+        public Weapon ()
+        {
+            Console.WriteLine("Set clip size:");
+            ClipSize = Convert.ToInt32(Console.ReadLine());
+            //if (IsValid == false)
+            //{
+            //    return;
+            //}
+            Console.WriteLine("Set your bullet count:");
+            BulletCount = Convert.ToInt32(Console.ReadLine());
+            if (IsValid == false)
+            {
+                return;
+            }
+            Console.WriteLine("Set seconds for discharge: ");
+            DischargeSeconds = Convert.ToInt32(Console.ReadLine());
+            if (IsValid == false)
+            {
+                return;
+            }
+            Console.WriteLine("Set fire mode(auto/single):");
+            ShootMode = Console.ReadLine().ToLower();
+            if (IsValid == false)
+            {
+                return;
+            }
+        }
+     }
+}
---
+++ WeaponTask/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading.Channels;
+using WeaponTask.Models;
+
+namespace WeaponTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+            Weapon[] weapons = new Weapon[1];
+            while (isRunning)
+            {
+                Console.WriteLine("Press:");
+                Console.WriteLine("g - create a new weapon");
+                Console.WriteLine("0 - get information about the current weapon");
+                Console.WriteLine("1 - shoot");
+                Console.WriteLine("2 - fire");
+                Console.WriteLine("3 - get remaining bullet count");
+                Console.WriteLine("4 - reload");
+                Console.WriteLine("5 - change fire mode");
+                Console.WriteLine("6 - terminate");
+
+                char key = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                
+
+                switch (key)
+                {
+                    case 'g':
+                        weapons[0] = new Weapon();
+                        if (weapons[0].IsValid == false)
+                        {
+                            break;
+                        }
+                        Console.WriteLine("New weapon created.");
+                        break;
+                    case '0':
+                        Console.WriteLine(weapons[0]);
+                        break;
+                    case '1':
+                        weapons[0].Shoot();
+                        break;
+                    case '2':
+                        weapons[0].Fire();
+                        break;
+                    case '3':
+                        Console.WriteLine(weapons[0].GetNeededBulletCount());
+                        break;
+                    case '4':
+                        weapons[0].Reload();
+                        break;
+                    case '5':
+                        weapons[0].ChangeFireMode();
+                        Console.WriteLine($"New shoot mode is {weapons[0].ShootMode}");
+                        break;
+                    case '6':
+                        isRunning = false;
+                        Console.WriteLine("Terminating...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
